2e148f7f4ecbc9ea00f63427283b965c1f4285cf
Barış Sokat <barissokat@outlook.com>
2022-01-13 16:33:51 +0300

    Add initial components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-function App () {
+import Cart from './components/Cart/Cart'
+import Layout from './components/Layout/Layout'
+import Products from './components/Shop/Products'
 
+function App () {
+  return (
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
+  )
 }
 
 export default App
