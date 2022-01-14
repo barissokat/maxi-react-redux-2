51e69f7ea464645be75bb8fdd555784473ce1715
Barış Sokat <barissokat@outlook.com>
2022-01-14 11:46:18 +0300

    Use an Action Creator Thunk

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { uiActions } from './store/ui-slice'
+import { sendCartData } from './store/cart-slice'
 
 import Cart from './components/Cart/Cart'
 import Layout from './components/Layout/Layout'
@@ -16,48 +16,13 @@ function App () {
   const notification = useSelector(state => state.ui.notification)
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!'
-        })
-      )
-
-      const response = await fetch('https://maxi-react-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
-        method: 'PUT',
-        body: JSON.stringify(cart)
-      })
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.')
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!'
-        })
-      )
-    }
-
     if (isInitial) {
       isInitial = false
       return
     }
 
-    sendCartData().catch(() => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!'
-        })
-      )
-    })
-  }, [cart])
+    dispatch(sendCartData(cart))
+  }, [cart, dispatch])
 
   return (
     <>
